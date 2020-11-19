af15dfef0d43df94cdf5cb67809e276da411593a
mishrasarthak08 <70634871+mishrasarthak08@users.noreply.github.com>
2020-11-19 04:45:12 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ dustbinmaterial1 = new Dustbin(1100,630,200,20);
 dustbinmaterial2 = new Dustbin(990,580,20,100);
 dustbinmaterial3 = new Dustbin(1210,580,20,100);
 
-paper = new Paper(100,645,30);
+paper = new Paper(300,645,30);
 
 	Engine.run(engine);
   
@@ -36,19 +36,18 @@ paper = new Paper(100,645,30);
 
 
 function draw() {
-  rectMode(CENTER);
+  rectMode(RADIUS);
   background(0);
   
  ground.display();
  dustbinmaterial1.display();
  dustbinmaterial2.display();
  dustbinmaterial3.display();
- keypressed();
  paper.display();
 }
-function keypressed(){
+function keyPressed(){
 if(keyCode===32){
-Matter.Body.applyForce(paper.body, paper.body.position, {x:1,y:-7});
+Matter.Body.applyForce(paper.body, paper.body.position, {x:170,y:-170});
 }
 }
 
